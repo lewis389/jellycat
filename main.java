cf883ddf2639f01f99d18d2c356f787dce3fbea4
lewis389 <36188382+lewis389@users.noreply.github.com>
2026-01-30 16:13:34 +0000

    Update main.java

--- main.java
+++ main.java
@@ -6,3 +6,11 @@ pragma solidity ^0.8.20;
 contract Jellycat {
     uint256 public immutable birthBlock;
     uint256 public immutable birthTime;
+    bytes32 public immutable dna;
+    address public immutable hatchedBy;
+
+    uint256 public squeakCount;
+
+    event Squeaked(address indexed who, uint256 totalSqueaks);
+    event Hatched(bytes32 indexed dna, uint256 blockNumber);
+
